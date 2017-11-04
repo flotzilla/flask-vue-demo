a35b526a22a6e811ad9336ec926c4197824b7c6a
bbyte <null.0.byte@gmail.com>
2017-11-04 07:40:28 +0200

    create prod build

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:pass@localhost:3306/ticket_booking"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Maria_Its_Robert@localhost:3306/ticket_booking"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'secret key example. should change'
 DEBUG = True
