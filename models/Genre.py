8b9620755ccc14a1f04dd721041005516b6b7e65
bbyte <null.0.byte@gmail.com>
2017-11-04 02:13:01 +0200

    upd models

---
+++ models/Genre.py
@@ -0,0 +1,9 @@
+from app import db
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
--- models/Movie.py
+++ models/Movie.py
@@ -1,15 +1,23 @@
 from app import db
+from models import Genre
+
+movie_genres_table = db.Table('movie_genre',
+                              db.Column('movie_id', db.Integer, db.ForeignKey('movie.id')),
+                              db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+                              )
 
 
 class Movie(db.Model):
     __tablename__ = 'movie'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    color = db.Column(db.String(100))
-    genre = db.Column(db.String(100))
-    poster_url = db.Column(db.String(256))
-    price = db.Column(db.Float(2))
-    duration = db.Column(db.Integer)
-    session_start = db.Column(db.TIMESTAMP)
-    session_date = db.Column(db.TIMESTAMP)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    poster_url = db.Column(db.String(256), nullable=False)
+    price = db.Column(db.Float(2), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    session_start = db.Column(db.TIMESTAMP, nullable=False)
+    session_date = db.Column(db.TIMESTAMP, nullable=False)
+
+    children = db.relationship("Genre",
+                               secondary=movie_genres_table)
